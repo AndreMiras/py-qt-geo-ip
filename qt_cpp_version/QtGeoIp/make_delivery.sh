bfde907ae6a266ad3c84c53487693a7d4f832cf4
Andre Miras <andre.miras@gmail.com>
2012-09-24 22:07:19 +0200

    added script to create tar.gz deliveries

---
+++ qt_cpp_version/QtGeoIp/make_delivery.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# This delivery script should be ran to compile and copy files to be delivered
+
+delivery_dir="delivery"
+delivery_sub_dir="$delivery_dir/delivery-`date +%Y-%m-%d`"
+ui_files="*.ui"
+header_files="*.h"
+source_files="*.cpp"
+docs_dir="docs/"
+nbproject_dir="nbproject/"
+nbproject_files="configurations.xml project.xml
+    qt-Debug.pro qt-Release.pro
+    qt-Debug.mk qt-Release.mk
+    Makefile-Debug.mk Makefile-Release.mk
+    Package-Debug.bash Package-Release.bash
+    Makefile-variables.mk Makefile-impl.mk Makefile"
+delivery_files="$ui_files $header_files $source_files"
+
+# cleans and makes
+make clean
+
+if [ ! -d $delivery_sub_dir ]
+then
+    mkdir -p $delivery_sub_dir
+fi
+
+if [ ! -d "$delivery_sub_dir/$nbproject_dir" ]
+then
+    mkdir -p "$delivery_sub_dir/$nbproject_dir"
+fi
+
+# copies relevant files
+cp -r $docs_dir $delivery_sub_dir
+cp $delivery_files $delivery_sub_dir
+for nbproject_file in $nbproject_files
+do
+    cp $nbproject_dir/$nbproject_file $delivery_sub_dir/$nbproject_dir
+done
+
+# makes a tar archive
+tar -cvzf $delivery_sub_dir.tar.gz $delivery_sub_dir
